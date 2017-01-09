a50755c6b143dc90b92b9460fa2ea2ebaa41af9e
Jarrod Chesney <jarrod.chesney@synerty.com>
2017-01-09 16:09:23 +1000

    Added comment about the ns module factory loader not working

--- nativescript.ts
+++ nativescript.ts
@@ -2,4 +2,8 @@
  * Created by peek on 9/01/17.
  */
 
+/*
+ * This doesn't work, it's getting compiler=null when it's used from this NPM package.
+ * If you copy the module-loader.ns.factory into the project, it works.
+ */
 export {PeekModuleFactoryLoader} from "./src/peek-web-ns/factories/module-loader.ns.factory";
